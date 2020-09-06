5721f44bd70074748f22a44ae7ebfe5b686f1ff4
jinbo-0523 <mi2ma3.0523@gmail.com>
2020-09-06 13:20:59 +0900

    コントローラーの処理をbefore_actionで共通化

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,12 @@
 class PostsController < ApplicationController
+
+  before_action :set_post, only: %i[show edit update destroy]
+
   def index
     @posts = Post.order(id: :asc)
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -17,23 +19,22 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
 
   end
 
   def update
-    post = Post.find(params[:id])
-    post.update!(post_params)
+    @post.update!(post_params)
     redirect_to post
 
   end
 
   def destroy
-    post =Post.find(params[:id])
-    post.destroy!
-    redirect_to post
+    @post.destroy!
+    redirect_to @post
   end
-
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
   private
   def post_params
